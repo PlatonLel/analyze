b8a12168255f5f08a4ea1ede29ffef12826b0690
Lelikov Platon Dmitrievich <pdlelikov@edu.hse.ru>
2025-01-03 18:19:26 +0300

    tested new func load_matrix

--- Source/test.cpp
+++ Source/test.cpp
@@ -1,13 +1,17 @@
 #include <Linalg.h>
 #include <Complex.h>
 #include <string>
+#include <filesystem>
+
 
 int main() {
     linalg::Matrix<Complex> matrix = {
             { Complex(1.0, 1.1), Complex(2.0, 0.0), Complex(0.0, -1.0) },
-            { Complex(0.00001, 0.0), Complex(3.0, -348193641.0), Complex(1.0, 1.00912) }
+            { Complex(0.1, 0.0), Complex(3.0, -3.0), Complex(1.0, 1.0) }
     };
-    linalg::Matrix<double> m = {{1.2,2.01,3.6},{1.023,2.6023,3.}};
-    std::cout << matrix;
+//    std::cout << "Current working directory: " << std::filesystem::current_path() << std::endl;
 
+    linalg::Matrix<double> m = {{1.2,2.01,3.6},{1.023,2.6023,3.}};
+//    std::cout << matrix;
+    std::cout << linalg::load_matrix("s.txt");
 }
